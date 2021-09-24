da2fb0e38b55500952ee6e6be1667f8cde9fad74
KhyatiNVaghela <khyatigohil@gmail.com>
2021-09-23 20:01:36 -0400

    Removed unnecessary code

--- WebDataScrapper.cs
+++ WebDataScrapper.cs
@@ -1,21 +1,4 @@
-﻿/*using System;
-
-namespace WebDataScrapper
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("\nWhat is your name? ");
-            var name = Console.ReadLine();
-            var date = DateTime.Now;
-            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
-            Console.Write("\nPress any key to exit...");
-            Console.ReadKey(true);
-        }
-    }
-}*/
+﻿
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
